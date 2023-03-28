0152b1f0b1cabb86d4d633e5416fd0e0dbecfcb7
Milan Nemec <milanemec@gmail.com>
2023-03-28 17:24:37 +0200

    docker, ws on different port

--- server/main.js
+++ server/main.js
@@ -4,40 +4,34 @@ const WebSocket = require('ws');
 
 const app = express();
 
-// Serve client-side code
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
 app.use(express.static('public'));
 
-// Create server
 const server = http.createServer(app);
-
-// Create WebSocket server
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({port: 3010 });
 
 // Handle WebSocket connection
 wss.on('connection', (ws) => {
-  console.log('WebSocket connected');
-
-  // Send message to client
-  ws.send('WebSocket connection established');
-
-  // Handle WebSocket message
-  ws.on('message', (message) => {
-    console.log(`Received message: ${message}`);
-  });
+    console.log('WebSocket connected');
+    ws.send('WebSocket connection established');
+    ws.on('message', (message) => {
+        console.log(`Received message: ${message}`);
+    });
 });
 
-// Expose endpoint
+
 app.get('/send-notification', (req, res) => {
-  // Send notification to browser
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send('notification');
-    }
-  });
-  res.send('Notification sent!');
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send('notification');
+        }
+    });
+    res.send('Notification sent!');
 });
 
-// Start server
-server.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(PORT, HOST, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
 });
